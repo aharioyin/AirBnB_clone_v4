634f8b590fe9fe2e189119e203ca46e2e1668ab8
MichaelDecent <michaeldecent2@gmail.com>
2023-10-09 16:13:20 +0000

    fixed the getting api endpoint place_search

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,36 +1,43 @@
-document.ready(function () {
+$(document).ready(function() {
 	const amenities = {};
-	$("li input[type=checkbox]").change(function () {
+	
+	$("input[type=checkbox]").on('change', function(){
+		let amenityId = $(this).data('id');
+		let amenityName = $(this).data('name');
+
 		if (this.checked) {
-			amenities[this.dataset.name] = this.dataset.id;
+			amenities[amenityId] = amenityName;
 		} else {
-			delete amenities[this.dataset.name];
+			delete amenities[amenityId];
 		}
-		$(".amenities h4").text(Object.keys(amenities).sort().join(", "));
+		$('.amenities h4').text(Object.values(amenities).join(', '));
 	});
-});
 
 	// Get status of API
-	$.getJSON("http://0.0.0.0:5001/api/v1/status/", (data) => {
-		if (data.status === "OK") {
-			$("div#api_status").addClass("available");
-		} else {
-			$("div#api_status").removeClass("available");
-		}
-	});
-});
+        $.ajax({
+                url: "http://0.0.0.0:5001/api/v1/status/",
+                type: "GET",
+                success: function(data) {
+                        if (data.status === "OK") {
+                                $("div#api_status").addClass("available");
+                        } else {
+                                $("#divapi_status").removeClass("available");
+                        }
+
+                }
+        });
 
 	//Fetch places
-	$.post({
-		url: `${HOST}/api/v1/places_search`,
-		data: JSON.stringify({}),
-		headers: {
-			"Content-Type": "application/json",
-		},
-		success: (data) => {
-			data.forEach((place) =>
-				$("section.places").append(
-					`<article>
+	$.ajax({
+		url: "http://127.0.0.1:5001/api/v1/places_search",
+		type: "POST",
+		data: "{}",
+		contentType: "application/json",
+		success: function (data) {
+			console.log(data);
+			for (let i = 0; i < data.length; i++) {
+				const place = data[i];
+				$("section.places").append(`<article>
 			<div class="title_box">
 			<h2>${place.name}</h2>
 			<div class="price_by_night">$${place.price_by_night}</div>
@@ -50,9 +57,8 @@ document.ready(function () {
 			${place.description}
 			</div>
 				</article>`
-				)
-			);
-		},
-		dataType: "json",
+				);
+			}
+		}
 	});
 });
